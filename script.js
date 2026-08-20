7a2ebc9238c7b3152e9a80861f370cbb1ee2cae2
Soumya Ranjan Nanda <148221309+SRNSoftware@users.noreply.github.com>
2026-08-20 11:00:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,7 @@
-function likePost() {
-    alert("Post liked!");
+function showMore(button) {
+  const paragraph = button.parentElement;
+  const moreText = paragraph.querySelector(".more-text");
+
+  moreText.style.display = "inline";
+  button.style.display = "none";
 }
-
-const postBtn = document.getElementById("postBtn");
-const postInput = document.getElementById("postText");
-const feed = document.getElementById("feed");
-
-postBtn.addEventListener("click", function () {
-
-    const text = postInput.value.trim();
-
-    if (text === "") {
-        alert("Please write something first!");
-        return;
-    }
-
-    const newPost = document.createElement("div");
-
-    newPost.className = "post";
-
-    newPost.innerHTML = `
-        <div class="post-header">
-            <img src="https://i.pravatar.cc/100?img=8">
-            <div>
-                <h3>Soumya Nanda</h3>
-                <small>Just now</small>
-            </div>
-        </div>
-
-        <div class="actions">
-            <button onclick="likePost()">❤️ Like</button>
-            <button>💬 Comment</button>
-            <button>🔄 Share</button>
-        </div>
-
-        <p class="caption">${text}</p>
-    `;
-
-    feed.prepend(newPost);
-
-    postInput.value = "";
-});
